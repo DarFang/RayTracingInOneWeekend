de221439cd56c5938630e090ed0f3fbfbcf85aa5
Darius Fang <dariusfang1@gmail.com>
2025-08-04 19:55:51 -0600

    refraction

--- sphere.h
+++ sphere.h
@@ -2,14 +2,15 @@
 #define SPHERE_H
 
 #include "hittable.h"
-#include "vec3.h"
+
 
 class sphere : public hittable {
   public:
-  sphere(const point3& center, double radius, shared_ptr<material> mat)
-  : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {        vec3 oc = center - r.origin();
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius*radius;
@@ -22,17 +23,16 @@ class sphere : public hittable {
 
         // Find the nearest root that lies in the acceptable range.
         auto root = (h - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {            
+        if (!ray_t.surrounds(root)) {
             root = (h + sqrtd) / a;
-            if (!ray_t.surrounds(root))                
-            return false;
+            if (!ray_t.surrounds(root))
+                return false;
         }
 
         rec.t = root;
+        rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
-        rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
         rec.mat = mat;
 
         return true;
@@ -43,5 +43,6 @@ class sphere : public hittable {
     double radius;
     shared_ptr<material> mat;
 };
+
 
 #endif
